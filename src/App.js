9b2d1f946938254b7627d70810320077882627e6
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-12 22:56:24 +1000

    Generate dynamic UI based off search results

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@ const App = () => {
             // pass the query of our search as a parameter 
             const data = await fetchWeather(query)
             
-            console.log(data)
+            setWeather(data);
+            setQuery('')
         }
     }
 
@@ -38,6 +39,25 @@ const App = () => {
             // onKeyPress - we will call our search function
             onKeyPress={search}
         />
+        {/* // AND operator 
+        // if weather.main is true... then show the proceeding code
+        // else do nothing */}
+        {weather.main && (
+            <div className='city'>
+                <h2 className='city-name'>
+                    <span>{weather.name}</span>
+                    <sup>{weather.sys.country}</sup>
+                </h2>
+                <div className='city-temp'>
+                    {Math.round(weather.main.temp)}
+                    <sup>&deg;C</sup>
+                </div>
+                <div className='info'>
+                    <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                    <p>{weather.weather[0].description}</p>
+                </div>
+            </div>
+        )}
     </div>
   )
 }
