8abf01044acc8c9e592e03807293d7361ae75f0f
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-12 22:41:39 +1000

    process explaination

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+// import React & useState hook from React
 import React, { useState } from 'react'
 
+// import API logic 
 import { fetchWeather } from './api/fetchWeather'
+// import styles 
 import './App.css'
 
+// This is our main functional component 
 const App = () => {
-    const [query, setQuery] = useState('')
 
+    // destructure our data coming from useState
+    const [query, setQuery] = useState('')
+    const [weather, setWeather] = useState({})
+
+    // async search function which excepts an event 
+    // check if the event.key = 'Enter' key 
     const search = async (e) => {
         if(e.key === 'Enter') {
+            // instantiate variable 'data' which = our API call 
+            // pass the query of our search as a parameter 
             const data = await fetchWeather(query)
-
+            
             console.log(data)
         }
     }
@@ -20,8 +31,11 @@ const App = () => {
             type="text" 
             className='search' 
             placeholder='Search...'
+            // query value = useState 
             value={query}
+            // onChange we useState to dynamically set our query
             onChange={(e) => setQuery(e.target.value)}
+            // onKeyPress - we will call our search function
             onKeyPress={search}
         />
     </div>
