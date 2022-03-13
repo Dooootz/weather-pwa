6d0395ce8befd9eb4f05a39962f3d52cbd7227da
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-13 14:38:13 +1000

    Initialize SW logic

---
+++ public/serviceworker.js
@@ -0,0 +1,30 @@
+// Create our cache name - cache is the storage of the browser 
+// for example - if we make a request to load an image...
+// we can store that data in our 'browser storage' 
+// next time we need that data, we can simply load it from the cache 
+// This is much faster & more effective 
+const CACHE_NAME = 'version-1'
+
+// store urls that we want to cache 
+// offline.html is the page that loads when their is no internet connection
+const urlsToCache = ['index.html', 'offline.html']
+
+// 'this' represents the SW
+const self = this;
+
+// Install SW 
+self.addEventListener('install', (event) => {
+
+});
+
+
+// Listen for requests 
+self.addEventListener('install', (event) => {
+
+});
+
+
+// Activate the SW
+self.addEventListener('install', (event) => {
+
+});
